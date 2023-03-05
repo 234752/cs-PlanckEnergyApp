78fbb33a79a5871871fdfd614756bb52ea460039
Patryk <patryk.gielcz@gmail.com>
2023-03-05 17:51:08 +0100

    new page, dropdown menus added

--- PlanckEnergyApp/PlanckEnergyMVC/Controllers/HomeController.cs
+++ PlanckEnergyApp/PlanckEnergyMVC/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Preparation()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
