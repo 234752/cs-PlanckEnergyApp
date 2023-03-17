d6e168425ea1d41b5d016de2a04170f42a3d6f68
Patryk <patryk.gielcz@gmail.com>
2023-03-17 19:18:18 +0100

    database model

---
+++ PlanckEnergyApp/PlanckEnergyMVC/Models/MaterialDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace PlanckEnergyMVC.Models;
+
+public class MaterialDbContext : DbContext
+{
+    public MaterialDbContext() : base("MaterialDbContext") { }
+
+    public DbSet<Material> Materials { get; set; }
+}
