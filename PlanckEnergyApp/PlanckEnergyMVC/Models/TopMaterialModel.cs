7c90df9e5f4db775b84b3494597de4b14527d5c7
Patryk <patryk.gielcz@gmail.com>
2023-05-13 16:31:33 +0200

    New page

---
+++ PlanckEnergyApp/PlanckEnergyMVC/Models/TopMaterialModel.cs
@@ -0,0 +1,20 @@
+﻿using PlanckEnergyMVC.DAL;
+
+namespace PlanckEnergyMVC.Models;
+
+public class TopMaterialModel
+{
+    private MaterialDbContext _dbContext;
+    private string _minedIngredients;
+    public string MinedIngredients
+    {
+        get { return _minedIngredients; }
+        set { _minedIngredients = value; }
+    }
+    private string _minedManufacturing;
+    public string MinedManufacturing
+    {
+        get { return _minedManufacturing; }
+        set { _minedManufacturing = value; }
+    }
+}
